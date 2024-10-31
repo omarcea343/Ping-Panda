6c570a551a136298f66d14eb78edc28ccab81b6f
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-10-31 11:57:44 -0600

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Heading } from "./components/heading"
 import { MaxWidthWrapper } from "./components/max-width-wrapper"
 import { Check } from "lucide-react"
+import { ShinyButton } from "./components/shiny-button"
 
 const Page = () => {
   return (
@@ -27,18 +28,27 @@ const Page = () => {
               sent directly to your Discord.
             </p>
 
-            <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-center sm:items-start">
+            <ul className="space-y-2 text-base/7 text-gray-600 text-left flex flex-col items-start">
               {[
                 "Real-time Discord alerts for critical events",
                 "Buy once, use forever",
                 "Track sales, new users, or any other event",
               ].map((item, index) => (
                 <li key={index} className="flex gap-1.5 items-center text-left">
-                  <Check className="size-5" />
+                  <Check className="size-5 shrink-0 text-brand-700" />
                   {item}
                 </li>
               ))}
             </ul>
+
+            <div className="w-full max-w-80">
+              <ShinyButton
+                href="/sign-up"
+                className="relative z-10 h-14 w-full text-base shadow-lg transition-shadow duration-300 hover:shadow-xl"
+              >
+                Start For Free Today
+              </ShinyButton>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
