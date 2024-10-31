e2bb5468653fc2b280a7b88b5f732a69f819c2a7
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-10-31 10:09:47 -0600

    Initial

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+const Page = () => {
+  return (
+    <>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
+  )
+}
+
+export default Page
