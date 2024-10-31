d6311445b6ff88bcca290bf777548647ae46ebb1
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-10-31 16:04:06 -0600

    Resources

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,6 +1,7 @@
-import { Plus } from "lucide-react"
+import { Inbox, Plus, User } from "lucide-react"
 import { PropsWithChildren } from "react"
 import { Icons } from "./icons"
+import Image from "next/image"
 
 export const MockDiscordUI = ({ children }: PropsWithChildren) => {
   return (
@@ -25,6 +26,44 @@ export const MockDiscordUI = ({ children }: PropsWithChildren) => {
         ))}
         <div className="group mt-auto size-12 bg-discord-background rounded-3xl flex items-center justify-center mb-3 hover:rounded-xl transition-all duration-200 hover:bg-[#3ba55c] cursor-not-allowed">
           <Plus className="text-[#3ba55c] group-hover:text-white" />
+        </div>
+      </div>
+
+      {/* dm list */}
+      <div className="hidden md:flex w-60 bg-[#2f3136] flex-col">
+        <div className="px-4 h-16 border-b border-[#202225] flex items-center shadow-sm">
+          <div className="w-full bg-[#202225] text-sm rounded px-2 h-8 flex items-center justify-center text-gray-500 cursor-not-allowed">
+            Find or start a conversation
+          </div>
+        </div>
+
+        <div className="flex-1 overflow-y-auto pt-4">
+          <div className="px-2 mb-4">
+            <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddde] cursor-not-allowed">
+              <User className="mr-4 size-8 text-[#b9bbbe]" />
+              <span className="font-medium text-sm">Friends</span>
+            </div>
+            <div className="flex items-center text-sm px-2 py-1.5 rounded hover:bg-[#393c43] text-[#dcddde] cursor-not-allowed">
+              <Inbox className="mr-4 size-8 text-[#b9bbbe]" />
+              <span className="font-medium text-sm">Nitro</span>
+            </div>
+          </div>
+
+          <div className="px-2 mb-4">
+            <h3 className="text-xs font-semibold text-[#8e9297] px-2 mb-2 uppercase">
+              Direct Messages
+            </h3>
+
+            <div className="flex items-center px-2 py-1.5 rounded bg-[#393c43] text-white cursor-pointer">
+              <Image
+                src="/brand-asset-profile-picture.png"
+                alt="PingPanda Avatar"
+                width={32}
+                height={32}
+                className="object-cover rounded-full mr-3"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
