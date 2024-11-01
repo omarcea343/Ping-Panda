bd2d9b0fa74645ad8de02644985da7fe58210e25
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-11-01 14:02:10 -0600

    Discord Demo Section

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,10 @@
-import { Check } from "lucide-react"
+import { DiscordMessage } from "@/components/discord-message"
 import { Heading } from "@/components/heading"
 import { MaxWidthWrapper } from "@/components/max-width-wrapper"
-import { ShinyButton } from "@/components/shiny-button"
 import { MockDiscordUI } from "@/components/mock-discord-ui"
+import { ShinyButton } from "@/components/shiny-button"
+import { AnimatedList } from "@/components/ui/animated-list"
+import { Check } from "lucide-react"
 
 const Page = () => {
   return (
@@ -59,7 +61,50 @@ const Page = () => {
         <div className="relative mx-auto">
           <MaxWidthWrapper className="relative">
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <MockDiscordUI></MockDiscordUI>
+              <MockDiscordUI>
+                <AnimatedList>
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="SignUp"
+                    badgeColor="#43b581"
+                    title="👤 New user signed up"
+                    content={{
+                      name: "Mateo Ortiz",
+                      email: "m.ortiz19@gmail.com",
+                    }}
+                  />
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PingPanda"
+                    timestamp="Today at 12:35PM"
+                    badgeText="Revenue"
+                    badgeColor="#faa61a"
+                    title="💰 Payment received"
+                    content={{
+                      amount: "$49.00",
+                      email: "zoe.martinez2001@email.com",
+                      plan: "PRO",
+                    }}
+                  />
+                  <DiscordMessage
+                    avatarSrc="/brand-asset-profile-picture.png"
+                    avatarAlt="PingPanda Avatar"
+                    username="PingPanda"
+                    timestamp="Today at 5:11PM"
+                    badgeText="Milestone"
+                    badgeColor="#5865f2"
+                    title="🚀 Revenue milestone reached"
+                    content={{
+                      recurringRevenue: "$5,000.00 USD",
+                      growth: "8.2%",
+                    }}
+                  />
+                </AnimatedList>
+              </MockDiscordUI>
             </div>
           </MaxWidthWrapper>
         </div>
