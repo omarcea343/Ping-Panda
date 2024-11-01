a6000734e320530ea3b993e137e00a3ec16effb5
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-11-01 12:10:09 -0600

    mock discord component

--- src/components/mock-discord-ui.tsx
+++ src/components/mock-discord-ui.tsx
@@ -1,4 +1,21 @@
-import { Cog, Headphones, Inbox, Mic, Plus, User } from "lucide-react"
+import {
+  Cog,
+  Gift,
+  Headphones,
+  HelpCircle,
+  Inbox,
+  Menu,
+  Mic,
+  Phone,
+  Pin,
+  Plus,
+  PlusCircle,
+  Search,
+  Smile,
+  Sticker,
+  User,
+  Video,
+} from "lucide-react"
 import { PropsWithChildren } from "react"
 import { Icons } from "./icons"
 import Image from "next/image"
@@ -100,7 +117,57 @@ export const MockDiscordUI = ({ children }: PropsWithChildren) => {
       <div className="flex-1 flex flex-col">
         {/* dm header */}
         <div className="h-16 bg-[#36393f] flex items-center px-4 shadow-sm border-b border-[#202225]">
-          <div>{/* TIME: 1:55:32 */}</div>
+          <div className="md:hidden mr-4">
+            <Menu className="size-6 text-[#b9bbbe] hover:text-white cursor-pointer" />
+          </div>
+
+          <div className="flex items-center">
+            <div className="relative">
+              <Image
+                src="/brand-asset-profile-picture.png"
+                alt="PingPanda Avatar"
+                width={40}
+                height={40}
+                className="object-cover rounded-full mr-3"
+              />
+              <div className="absolute bottom-0 right-3 size-3 bg-green-500 rounded-full border-2 border-[#36393f]" />
+            </div>
+
+            <p className="font-semibold text-white">PingPanda</p>
+          </div>
+
+          <div className="ml-auto flex items-center space-x-4 text-[#b9bbbe]">
+            <Phone className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Video className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Pin className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <User className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Search className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <Inbox className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            <HelpCircle className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+          </div>
+        </div>
+
+        {/* message history */}
+        <div className="flex-1 overflow-y-auto p-4 bg-discord-background flex flex-col-reverse">
+          {children}
+        </div>
+
+        {/* message input */}
+        <div className="p-4">
+          <div className="flex items-center bg-[#40444b] rounded-lg p-1">
+            <PlusCircle className="mx-3 text-[#b9bbbe] hover:text-white cursor-not-allowed" />
+            <input
+              readOnly
+              type="text"
+              placeholder="Message @PingPanda"
+              className="flex-1 bg-transparent py-2.5 px-1 text-white placeholder-[#72767d] focus:outline-none cursor-not-allowed"
+            />
+            <div className="flex items-center space-x-3 mx-3 text-[#b9bbbe]">
+              <Gift className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+              <Sticker className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+              <Smile className="size-5 hover:text-white cursor-not-allowed hidden sm:block" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
