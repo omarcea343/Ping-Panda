bbbfdc8f75c5be984322102c4600b8d5571da92e
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-11-08 23:43:39 -0600

    A

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,22 @@
-const Page = () => {
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const Page = async () => {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect("/sign-in")
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    redirect("/sign-in")
+  }
+
   return <div>Dashboard</div>
 }
 
