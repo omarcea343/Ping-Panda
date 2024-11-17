05b0f93efe979989eebe01cc5b9563aeb111ce9f
Miguel Omar Zúñiga Rivera <miguel_omar30@hotmail.com>
2024-11-16 19:24:29 -0600

    Dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { DashboardPage } from "@/components/dashboard-page"
 import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
@@ -17,7 +18,7 @@ const Page = async () => {
     redirect("/sign-in")
   }
 
-  return <div>Dashboard</div>
+  return <DashboardPage title="Dashboard">Dashboard page</DashboardPage>
 }
 
 export default Page
