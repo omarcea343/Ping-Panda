224cff05227501b37530d13a0bd8498c8314450a
Omar Zuniga <miguel_omar30@hotmail.com>
2025-01-23 16:48:36 -0600

    Auth

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,13 @@ import { MaxWidthWrapper } from "./max-width-wrapper"
 import { SignOutButton } from "@clerk/nextjs"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
+import { currentUser } from "@clerk/nextjs/server"
 
-export const Navbar = () => {
-  const user = false
+export const Navbar = async () => {
+  const user = await currentUser()
 
   return (
-    <nav className="sticky z=[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-16 items-center justify-between">
           <Link href="/" className="flex z-40 font-semibold">
